4f6fe487771dc7604af86fee7b597dc320515fc3
unknown <Ivan Fernolendt@EN610932.endava.net>
2022-08-11 17:14:23 +0300

    Prima incarcare, nu sunt testele

---
+++ src/test/java/com/javap/proiect/jpa/ProiectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javap.proiect.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProiectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
